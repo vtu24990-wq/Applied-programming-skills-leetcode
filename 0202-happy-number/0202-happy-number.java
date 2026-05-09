148e7bc38daf14e58eeeb2dab5b427b124e68571
vtu24990-wq <vtu24990@veltech.edu.in>
2026-05-09 21:28:44 +0530

    Time: 2 ms (17.64%), Space: 42.8 MB (12.81%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> seen = new HashSet<>();
+        
+        // Loop until n is 1 or we find a cycle
+        while (n != 1 && !seen.contains(n)) {
+            seen.add(n);
+            n = getNext(n);
+        }
+        
+        return n == 1;
+    }
+
+    private int getNext(int n) {
+        int totalSum = 0;
+        while (n > 0) {
+            int d = n % 10;
+            n = n / 10;
+            totalSum += d * d;
+        }
+        return totalSum;
+    }
+}
