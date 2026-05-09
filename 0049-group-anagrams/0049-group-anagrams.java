3c8bfdcc5aa8202ca86c86bb9a073b653c0182fe
vtu24990-wq <vtu24990@veltech.edu.in>
2026-05-09 21:28:09 +0530

    Time: 6 ms (99.52%), Space: 49.8 MB (46.56%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null || strs.length == 0) return new ArrayList<>();
+        
+        Map<String, List<String>> map = new HashMap<>();
+        
+        for (String s : strs) {
+            // Sort the string to create a key
+            char[] ca = s.toCharArray();
+            Arrays.sort(ca);
+            String key = String.valueOf(ca);
+            
+            // Add to the corresponding list in the map
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(s);
+        }
+        
+        return new ArrayList<>(map.values());
+    }
+}
